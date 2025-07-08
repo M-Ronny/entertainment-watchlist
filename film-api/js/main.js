677c88d910d24cdda5d1623a46ad5403971e3192
Ronaldo <ronaldomorales.ai@gmail.com>
2025-07-08 16:38:18 -0400

    updated index.html and created element to grab title, year and img if any from the data

--- film-api/js/main.js
+++ film-api/js/main.js
@@ -10,15 +10,21 @@ function getFetch(){
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        if(data.media_type === "image"){
-          document.querySelector('img').src = data.hdurl
-        }else if(data.media_type === 'video'){
-          document.querySelector('iframe').src = data.url
-        }else{
-          alert('Media Not Supported - Contact NASA IMMEDIATLY')
+        for (let i = 0; i < data.description.length; i++) {
+          const title = document.createElement('h1')
+          const h3 = document.createElement('h3')
+          const img = document.createElement('img')
+
+          title.classList.add('title')
+
+          title.textContent = data.description[i]['#TITLE']
+          h3.textContent = data.description[i]['#YEAR']
+          img.src = data.description[i]['#IMG_POSTER']
+
+          document.querySelector('body').appendChild(title)
+          document.querySelector('body').appendChild(h3)
+          document.querySelector('body').appendChild(img)
         }
-       
-        document.querySelector('h3').innerText = data.explanation
       })
       .catch(err => {
           console.log(`error ${err}`)
