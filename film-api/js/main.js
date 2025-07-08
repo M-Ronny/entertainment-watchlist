ea847a1afdb56861f46f0a8bffb921de2a88deb9
Ronaldo <ronaldomorales.ai@gmail.com>
2025-07-08 16:52:25 -0400

    updated index.html and main.js

--- film-api/js/main.js
+++ film-api/js/main.js
@@ -10,6 +10,7 @@ function getFetch(){
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        document.querySelector('.result').textContent = ''
         for (let i = 0; i < data.description.length; i++) {
           const title = document.createElement('h1')
           const h3 = document.createElement('h3')
@@ -21,9 +22,9 @@ function getFetch(){
           h3.textContent = data.description[i]['#YEAR']
           img.src = data.description[i]['#IMG_POSTER']
 
-          document.querySelector('body').appendChild(title)
-          document.querySelector('body').appendChild(h3)
-          document.querySelector('body').appendChild(img)
+          document.querySelector('.result').appendChild(title)
+          document.querySelector('.result').appendChild(h3)
+          document.querySelector('.result').appendChild(img)
         }
       })
       .catch(err => {
